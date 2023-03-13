f7cdb657230e55cd5a206b05e321d162feeef1d6
AxelColliaux <axel.colliaux@hotmail.fr>
2023-03-13 11:01:26 +0100

    challenge ok

---
+++ src/main/java/fr/wcs/security/controller/HomeController.java
@@ -0,0 +1,23 @@
+package fr.wcs.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "Welcome to the SHIELD";
+    }
+
+    @GetMapping("/avengers/assemble")
+    public String champion(){
+        return "Avengers... Assemble";
+    }
+
+    @GetMapping("/secret-bases")
+    public String director(){
+        return "Biarritz, Bordeaux, La Loupe, Lille, Lyon, Nantes, Orléans, Paris, Reims, Strasbourg, Toulouse, Amsterdam, Barcelone, Berlin, Lisbonne, Madrid";
+    }
+}
